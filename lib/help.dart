a784d39a3286cbf81382529763274db868a14521
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-02-29 22:30:17 +0100

    Finishing animating

--- lib/help.dart
+++ lib/help.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 class HelpStack extends StatefulWidget {
   const HelpStack({super.key});
 
@@ -7,6 +10,34 @@ class HelpStack extends StatefulWidget {
 }
 
 class _HelpStackState extends State<HelpStack> {
+  List<Widget> cards = [];
+  List<Map<String, dynamic>> items = [
+    {
+      'text': 'Clica en la noticia para ir a la web',
+      'icon': Icons.touch_app_outlined
+    },
+    {
+      'text': 'Doble click',
+      'icon': Icons.touch_app
+    },
+    {
+      'text': 'Triple click',
+      'icon': Icons.touch_app
+    },
+  ];
+  @override
+  void initState() {
+    for (int i = 0; i < items.length; i++) {
+      Timer(Duration(seconds: 5 * (i + 1)), () {
+        setState(() {
+          cards.add(
+            HelpItem(text: items[i]['text'], iconData: items[i]['icon'],),
+          );
+        });
+      });
+    }
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,12 +46,9 @@ class _HelpStackState extends State<HelpStack> {
       ),
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          HelpItem(text: 'Clica en la noticia para ir a la web', iconData: Icons.touch_app_outlined,)
-        ],
+      child: Stack(
+        alignment: Alignment.center,
+        children: cards,
       ),
     );
   }
@@ -36,26 +64,54 @@ class HelpItem extends StatefulWidget {
 }
 
 class _HelpItemState extends State<HelpItem> {
+  final Duration duration = const Duration(seconds: 5);
+  double opacity = 0.0;
+  double positionX = 70;
   @override
   void initState() {
-
+    SchedulerBinding.instance.addPostFrameCallback((_){
+      setState(() {
+        positionX = -70;
+        opacity = 1.0;
+      });
+    });
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        AnimatedPositioned(
-          duration: const Duration(seconds: 1),
-
-          child: Column(
-            children: [
-              Icon(widget.iconData, size: 200, color: Colors.white70,),
-              Text(widget.text, style: const TextStyle(color: Colors.white, fontSize: 18.0),)
-            ],
-          )
-        ),
-      ],
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedPositioned(
+            duration: duration,
+            curve: Curves.easeInQuint,
+            right: positionX,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: AnimatedOpacity(
+              opacity: opacity,
+              duration: const Duration(milliseconds: 2500),
+              onEnd: (){
+                setState(() {
+                  opacity = 0.0;
+                });
+              },
+              curve: Curves.easeInSine,
+              child: Column(
+                children: [
+                  const Expanded(child: SizedBox.shrink(),),
+                  Icon(widget.iconData, size: 200, color: Colors.white70,),
+                  Text(widget.text, style: const TextStyle(color: Colors.white, fontSize: 18.0),),
+                  const Expanded(child: SizedBox.shrink(),),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
