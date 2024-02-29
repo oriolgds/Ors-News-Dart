9cf966f109a02c7662cdecdca7a78b603dab7c9a
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-02-29 20:21:54 +0100

    Building stack

--- lib/help.dart
+++ lib/help.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
-class HelpStack extends StatelessWidget {
+class HelpStack extends StatefulWidget {
   const HelpStack({super.key});
 
+  @override
+  State<HelpStack> createState() => _HelpStackState();
+}
+
+class _HelpStackState extends State<HelpStack> {
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.black.withAlpha(50),
+        color: Colors.black.withAlpha(230),
       ),
-      child: Stack(
-        children: <Widget>[
-
-        ]
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          HelpItem(text: 'Clica en la noticia para ir a la web', iconData: Icons.touch_app_outlined,)
+        ],
       ),
     );
   }
 }
+
+class HelpItem extends StatelessWidget {
+  const HelpItem({super.key, required this.text, required this.iconData});
+  final String text;
+  final IconData iconData;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(iconData, size: 200, color: Colors.white70,),
+        Text(text, style: const TextStyle(color: Colors.white, fontSize: 18.0),)
+      ],
+    );
+  }
+}
+
