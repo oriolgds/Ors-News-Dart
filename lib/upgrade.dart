640f223ed84aacb630029e641498c2acbb2c5bf2
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-03-22 22:39:50 +0100

    Getting app version local

--- lib/upgrade.dart
+++ lib/upgrade.dart
@@ -4,6 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
+
+import 'package:package_info_plus/package_info_plus.dart';
+
 Future<void> upgrade() async {
   // Get the release url
   final response = await http.get(Uri.parse('https://api.github.com/repos/oriolgds/Ors-News-Dart/releases'));
@@ -17,9 +20,17 @@ Future<void> upgrade() async {
   for (final asset in jsonRelease){
     if(asset['name'] == 'app-release.apk'){
       debugPrint(asset['browser_download_url']);
-      downloadTheFile(asset['browser_download_url']);
+      compareVersions('');
+      //downloadTheFile(asset['browser_download_url']);
     }
   }
+}
+
+Future<bool> compareVersions(String cloudVersion) async {
+  final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  String version = packageInfo.version;
+  debugPrint("The version is: $version");
+  return true;
 }
 
 Future<void> downloadTheFile(String url) async {
